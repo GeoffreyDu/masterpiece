1d92ec7e04d33775f82dd7df986305d5c9860c2a
GeoffreyDu <geoffrey.ducci@gmail.com>
2020-01-25 20:42:07 +0100

    finish user crud

---
+++ masterpiece-back/src/main/java/fr/formation/masterpieceback/controllers/UserController.java
@@ -0,0 +1,39 @@
+package fr.formation.masterpieceback.controllers;
+
+import fr.formation.masterpieceback.dtos.UserDto;
+import fr.formation.masterpieceback.entities.User;
+import fr.formation.masterpieceback.services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    protected void create(@Valid @RequestBody UserDto dto){
+        userService.create(dto);
+    }
+
+    @GetMapping("/{id}")
+    protected User getOne(@PathVariable("id") Long id) {
+        return userService.getOne(id);
+    }
+
+    @PutMapping("/{id}")
+    protected void update(@PathVariable("id") Long id, @Valid @RequestBody UserDto dto){
+        userService.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    protected void delete(@PathVariable("id") Long id) {
+        userService.delete(id);
+    }
+
+}
